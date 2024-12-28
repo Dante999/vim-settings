755764107124e878f4a4cdf5474dc017ef6164df
Dante999 <dante999@users.noreply.github.com>
2024-12-28 11:48:42 +0100

    Add plugin for plantuml syntax

--- init_plugins.sh
+++ init_plugins.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-git clone https://github.com/vim-airline/vim-airline bundle/vim-airline
-git clone https://github.com/morhetz/gruvbox.git bundle/gruvbox
-git clone https://github.com/preservim/tagbar.git bundle/tagbar
-git clone https://github.com/preservim/nerdtree.git bundle/nerdtree
-git clone https://github.com/dense-analysis/ale.git bundle/ale
-git clone https://github.com/martinda/Jenkinsfile-vim-syntax.git
-git clone https://github.com/maralla/completor.vim.git bundle/completor.vim
-git clone https://github.com/MattesGroeger/vim-bookmarks.git ~/.vim/bundle/vim-bookmarks
-git clone https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/bundle/ctrlp
+pushd bundle
 
+git clone https://github.com/vim-airline/vim-airline
+git clone https://github.com/morhetz/gruvbox.git
+git clone https://github.com/preservim/tagbar.git
+git clone https://github.com/preservim/nerdtree.git
+git clone https://github.com/dense-analysis/ale.git
+git clone https://github.com/martinda/Jenkinsfile-vim-syntax.git
+git clone https://github.com/maralla/completor.vim.git
+git clone https://github.com/MattesGroeger/vim-bookmarks.git
+git clone https://github.com/ctrlpvim/ctrlp.vim.git
+git clone https://github.com/aklt/plantuml-syntax.git
+
+popd
