0c8236f3d2b74552486148a8d2a6a8e01078a5dc
Matthias Escher <m.escher@abus-sc.com>
2025-03-27 10:10:13 +0100

    Add habamax and xamabah colorschemes for older vim versions

--- init_colors.sh
+++ init_colors.sh
@@ -3,5 +3,7 @@
 pushd colors
 
 curl -O https://raw.githubusercontent.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim
+curl -O https://raw.githubusercontent.com/habamax/vim-habamax/refs/heads/master/colors/habamax.vim
+curl -O https://raw.githubusercontent.com/habamax/vim-habamax/refs/heads/master/colors/xamabah.vim
 
 popd
